2f7442e6444ee118cd7de63c724cd8be53f1c202
Emre <72259197+4Forcas@users.noreply.github.com>
2022-02-15 14:59:16 +0100

    Initial

---
+++ PXLPro2022Shoppers07/PXLPro2022Shoppers07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PXLPro2022Shoppers07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
