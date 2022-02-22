855a374c7d939f7336c4aa3de0f7ca81b2120cb0
warreheeren <warre.heeren@hotmail.com>
2022-02-22 15:21:21 +0100

    Web API toegevoegd + dbcontext, etc opgezet.

---
+++ PXLPro2022Shoppers07/Shopper07WebAPI/Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopper07WebAPI.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
